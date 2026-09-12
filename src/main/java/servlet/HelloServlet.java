5ad30a82a3bae7e955d961fb68c0904ffa7b39b8
Namrata Kundu <namratakundu22work@gmail.com>
2026-09-13 02:06:42 +0530

    add tomcat maven setup

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws IOException {
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>Hello! Placement System is working.</h1>");
+    }
+}
